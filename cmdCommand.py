860733b1c8349a5ff5f66c621cca36718cff5741
jo ny <johnjony2525@gmail.com>
2026-09-08 11:12:41 +0530

    Add files via upload

---
+++ cmdCommand.py
@@ -0,0 +1,18 @@
+
+(.venv) C:\PneumoniaAI>C:\Users\jony\PneumoniaAI\.venv\Scripts\activate
+streamlit run app.py
+
+
+
+
+cd C:\PneumoniaAI
+.venv\Scripts\activate
+
+.venv\Scripts\activate
+
+
+
+
+
+
+7-nz*Q*G#2RnfqU = password
